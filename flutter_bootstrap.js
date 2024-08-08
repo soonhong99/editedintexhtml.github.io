f31ac63f98c56c3d8af327e869e601dd18f6d353
soonhong99 <67502252+soonhong99@users.noreply.github.com>
2024-08-09 00:27:21 +0900

    update: UI, retry in ready screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1017802666"
+    serviceWorkerVersion: "3685346282"
   }
 });
